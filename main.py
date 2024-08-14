c7c6ab9640ede38b7f2d9b7ede628a3e070b476c
Nicolás Zarantonello <124157205+NicoZdev@users.noreply.github.com>
2024-08-14 20:31:53 +0000

    Pasos usar git en main.py rama pruebas

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from gui import create_main_window
 # Punto de entrada de la aplicación
 if __name__ == "__main__":
     create_main_window()
+
+
+#Para utilizar git correctamente:
+#git checkout nombreRama (para movernos entre las ramas existentes, en este caso utilizariamos "main" o "pruebas")
+#Para hacer el commit vamos a control de codigo y dejamos el comentario de lo editado y lo enviamos.
