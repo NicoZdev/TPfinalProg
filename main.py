0d05fd2b108d2e8c584a3d448070726881c71b40
Nicolás Zarantonello <124157205+NicoZdev@users.noreply.github.com>
2024-08-14 17:43:06 -0300

    pasos git main.py

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ if __name__ == "__main__":
 
 
 #Para utilizar git correctamente:
-#git checkout nombreRama (para movernos entre las ramas existentes, en este caso utilizariamos "main" o "pruebas")
+#git checkout -b nombreRama (cramos una rama para hacer modificaciones de prueba que luego implementaremos en la rama principal)
+#git checkout nombreRama (para movernos entre las ramas existentes, en este caso utilizariamos "main" y asi verificar que contiene)
 #Para hacer el commit vamos a control de codigo y dejamos el comentario de lo editado y lo enviamos.
