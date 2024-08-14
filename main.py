11b9524ff6c8689eedce0cf457922916fe528736
Nicolás Zarantonello <124157205+NicoZdev@users.noreply.github.com>
2024-08-14 20:52:36 +0000

    comentarios

--- main.py
+++ main.py
@@ -10,5 +10,4 @@ if __name__ == "__main__":
 #git checkout -b nombreRama (cramos una rama para hacer modificaciones de prueba que luego implementaremos en la rama principal)
 #git checkout nombreRama (para movernos entre las ramas existentes, en este caso utilizariamos "main" y asi verificar que contiene)
 #Para hacer el commit vamos a control de codigo y dejamos el comentario de lo editado y lo enviamos.
-
-#asfkjaskljfaslk
+#Al terminar hacer el pull request desde github, para posteriormente ser aceptado en la rama main.
